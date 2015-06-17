ec63a9f004428d35726038ece5e0f799f118957f
Herald <heraldfreire@gmail.com>
2015-06-17 06:24:10 -0300

    1.0.1.2

--- exercises_ruby/03_exercise/01_quest.rb
+++ exercises_ruby/03_exercise/01_quest.rb
@@ -6,3 +6,27 @@ Atributos: Cor, circunferência, material Métodos: trocaCor e mostraCor
 =end
 
 
+class Bola
+	attr_accessor :cor, :circunferencia, :material
+	def cor
+		puts "Vermelha"
+	end
+
+	def mostra_cor
+		puts @cor
+	end
+
+	def troca_cor (cor)
+		@cor = cor
+
+	end
+end
+
+
+bola = Bola.new
+
+bola.cor
+bola.troca_cor("Azul")
+bola.mostra_cor
+bola.troca_cor("Verde")
+bola.mostra_cor
