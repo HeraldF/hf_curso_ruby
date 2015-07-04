2596ed61cca8516934c11939b6fcc034fe536aa5
Herald Freire <heraldfreire@gmail.com>
2015-07-04 17:52:49 -0300

    1.0.1.4

--- apps_rails/blog/db/schema.rb
+++ apps_rails/blog/db/schema.rb
@@ -11,13 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620182547) do
+ActiveRecord::Schema.define(version: 20150704194551) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text   "Body"
+    t.string "name"
+    t.string "email"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id"
   end
 
 end
