12eae0a606db52e355e7e72408824b889eceddf3
Herald Freire <heraldfreire@gmail.com>
2015-08-01 15:08:57 -0300

    CRUD Author 01/08/15

--- apps_rails/blog/app/controllers/authors_controller.rb
+++ apps_rails/blog/app/controllers/authors_controller.rb
@@ -1,5 +1,55 @@
 class AuthorsController < ApplicationController
+
+	before_action :set_author, only: [:show, :edit, :destroy, :update]
+	
+	#GET /authors
 	def index
-	@authors = Author.all
+		@authors = Author.all
+	end
+
+	#GET /authors/new
+	def new
+		@author = Author.new
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	#POST /authors
+	def create
+		@author = Author.new(author_params)
+		if @author.save
+			redirect_to authors_path, :notice => "Author #{author_params[:name]} registered sucessfully!"
+		else
+			flash[:notice] = "Athor can not registered. Fill in the empty fiels!"
+			render :new
+		end	
+	end
+
+	def update
+		if @author.update_attributes(author_params)
+			redirect_to authors_path, :notice => "Author #{@author.name} update successfully"		
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@author.destroy
+		redirect_to authors_path, :notice => "Author delete successfully"
+	end
+
+
+	private
+
+	def author_params
+		params.require(:author).permit(:name, :bio)
+	end
+
+	def set_author
+		@author = Author.find(params[:id])
 	end
 end
